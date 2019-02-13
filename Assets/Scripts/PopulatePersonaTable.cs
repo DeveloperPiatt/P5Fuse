7d6328c2a69dd33951dcee8c3380e762e7141946
Nick Piatt <developer.piatt@gmail.com>
2019-02-12 16:31:44 -0800

    eod checkin

---
+++ Assets/Scripts/PopulatePersonaTable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulatePersonaTable : MonoBehaviour
+{
+
+    public int ROWCOUNT;
+    public GameObject personaTableRow;
+    public GameObject scrollView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=0; i< ROWCOUNT; i++) {
+            Debug.Log("testing");
+             GameObject go1 = (GameObject)Instantiate(personaTableRow, Vector3.zero, Quaternion.identity);
+             go1.transform.SetParent(scrollView.transform, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
